0a363943063d46633e1ce2f3e3fe6dec282c48ea
Christian dos Santos Figueiró <christian.dsantos@outlook.com>
2021-06-18 23:10:56 -0300

    Arrumei alguns links e algumas cores secundárias do site, organizando pastas e CSS, adicionando links dos sites nos projetos e fazendo breve exemplo de página das matérias do blog.

---
+++ assets/js/fecharMenu.js
@@ -0,0 +1,13 @@
+/*
+$('body').on('click', function(e) {
+    var $this = $(this);
+    e.preventDefault();
+
+    if ( $('nav').hasClass('menu-open') ) {
+        $('nav').removeClass('menu-open');
+        $this.removeClass('active');
+    } else {
+        
+    }
+})
+*/
